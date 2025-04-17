18122a3f7cc2467c9df17610d74215ccb73af880
ADITI DALVI <Aditi.Dalvi@ibm.com>
2025-04-17 09:47:36 +0000

    commit

--- models/cs1/product.sql
+++ models/cs1/product.sql
@@ -5,9 +5,5 @@ SELECT
     t1.Price,
     t1.Stock
 FROM MYDB.ONEDRIVE.PRODUCT_DATA_SHEET_1 AS t1
-INNER JOIN MYDB.POSTGRESCS_PUBLIC.STG_PRODUCTS_2 AS t2
+INNER JOIN MYDB.POSTGRESCS_PUBLIC.STG_PRODUCTS AS t2
     ON t1.Product_ID = t2.Product_ID
-    AND t1.Product_Name = t2.Product_Name
-    AND t1.Category = t2.Category
-    AND t1.Price = t2.Price
-    AND t1.Stock = t2.Stock
